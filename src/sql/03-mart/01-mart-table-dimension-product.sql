c95ada7ab267a6d0836538e357cb4b376afdb6fa
bmcpl <81463616+lustrousiana@users.noreply.github.com>
2026-09-02 12:52:05 +0800

    Modify dim_product SQL to handle NULL values
    Updated joins to LEFT JOIN and mapped NULLs to 'Unknown'.

--- src/sql/03-mart/01-mart-table-dimension-product.sql
+++ src/sql/03-mart/01-mart-table-dimension-product.sql
@@ -1,12 +1,12 @@
--- create dim_product (flattened: aisle + department joined in)
+-- create dim_product (flattened: aisle + department joined in, NULLs mapped to "Unknown")
 CREATE OR REPLACE TABLE `ftw-week-06`.`03-mart`.dim_product AS
 SELECT
     p.product_id,
     p.product_name,
-    a.aisle,
-    d.department
+    COALESCE(a.aisle, 'Unknown') AS aisle,
+    COALESCE(d.department, 'Unknown') AS department
 FROM `ftw-week-06`.`02-clean`.products_clean p
-JOIN `ftw-week-06`.`02-clean`.aisles_clean a
+LEFT JOIN `ftw-week-06`.`02-clean`.aisles_clean a
     ON p.aisle_id = a.aisle_id
-JOIN `ftw-week-06`.`02-clean`.departments_clean d
+LEFT JOIN `ftw-week-06`.`02-clean`.departments_clean d
     ON p.department_id = d.department_id;
